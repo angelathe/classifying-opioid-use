09b7cf6477a9602973daa83a64a7fcea7f4df24f
Matthew Kaufmann <matthew.e.kaufmann@gmail.com>
2022-05-11 17:09:42 -0500

    Revised data_clean_pmf

--- data_cleaning/data_clean_pmf.py
+++ data_cleaning/data_clean_pmf.py
@@ -28,40 +28,47 @@ def read_and_combine(data_to_read, years_to_read):
 
     col_dict_clean = {}
     for _, row in pmf_cols_clean.iterrows():
-        col_dict[row[0]] = row[1]
+        col_dict_clean[row[0]] = row[1]
     
-    # Note: ndc opiod label from NYS for taxation purposes - going to see if this works
-    # Only has first 9 digits as it excludes final 2 for dosage size, so will match on 1st 9
-    # https://health.ny.gov/professionals/narcotic/docs/opioid_drug_listing.pdf
+    # CDC opioid agonist lists
+    # https://ndclist.com/pharma-class/
+    # search for opioids, combine 3 lists
     opioid_ndc = pd.read_csv('NDC_opioids.csv', dtype={0: 'str'})
-    # opioid_ndc.insert(1, column='first_ndc_string', value=opioid_ndc['NDC'].str.split('-').str[0])
-    # opioid_ndc.insert(2, column='second_ndc_string', value=opioid_ndc['NDC'].str.split('-').str[1])
     opioid_ndc.insert(1, column='NDC_clean', value=opioid_ndc['NDC'].str.replace('-', ""))
     opiod_ndc_prescribed = opioid_ndc[opioid_ndc['Product Type'] == 'Human Prescription Drug']
     opioid_codes = set(opiod_ndc_prescribed['NDC_clean'])
 
+    df_final = pd.DataFrame()
+
     for i, file in enumerate(data_to_read):
         df = pd.read_csv(file)
-        # get desired columns and insert YEAR and IDsssssss
+        # get desired columns and insert YEAR and IDs
         df_new = df[col_dict.keys()]
         year_value = years_to_read[i]
         df_new.insert(loc = 0, column = 'YEAR', value = year_value)
         df_new.insert(loc = 0, column = 'ID', value = df_new['YEAR'] + '_' + df_new['DUPERSID'].astype(str))
-
-    # replace negatives with NaN
-    # NOTE still in progress as Wes's files are making me redo because it reads in number + text sometimes
-        df_new.loc[df_new['PREGCAT'].apply(lambda x: np.nan if x.isdigit else x)]
-        df_new.loc[df_new['RXFORM'].apply(lambda x: np.nan if x.isdigit else x)]
+        # replace negatives with NaN
+        # NOTE still in progress as Wes's files are making me redo because it reads in number + text sometimes
+        df_intermediate = df_new.copy()
+        for col in ['PREGCAT', 'RXFORM']:
+            df_intermediate[col] = df_new.loc[:, (col)].apply(lambda x: np.nan if x == "-15" or x == "-9" else x)
+        df_intermediate['RXNDC'] = df_new.loc[:, ('RXNDC')].apply(lambda x: np.nan if x == -15 else x)
         for col in ['RXDAYSUP', 'PHARTP1', 'RXBEGYRX']:
-            df_new.loc[df_new[col].apply(lambda x: int(x) if int(x.split(" ")[0]) > 0 else np.nan)]
+            df_intermediate[col] = df_new.loc[:, (col)].apply(lambda x: int(x.split(" ")[0]) if int(x.split(" ")[0]) > 0 else np.nan)
 
-    # Change year prescribed to years since taken dose - note some are 2020 for 2019 so do a max and count as 0 for current year
+        # Change year prescribed to years since taken dose - note some are 2020 for 2019 so do a max and count as 0 for current year
+        df_intermediate2 = df_intermediate.copy()
+        year_value = int(year_value)
+        df_intermediate2['RXBEGYRX'] = df_intermediate['RXBEGYRX'].apply(lambda x: max(year_value-int(x), 0) if x > 0 else x)
 
-    year_value = int(year_value)
-    df_new['RXBEGYRX'] = df_new['RXBEGYRX'].apply(lambda x: max(year_value-int(x), 0) if x != np.nan and int(x) > 0 else x)
+        ##### ONLY GETTING LIKE 30 OPIOD_PRES...
+        df_intermediate2 = df_intermediate2.assign(OPIOID_PRESCRIBED = df_new['RXNDC'].apply(lambda x: 1 if str(x)[0:9] in opioid_codes or str(x)[0:10] in opioid_codes else 0))
 
-    ##### ONLY GETTING LIKE 30 OPIOD_PRES...
-    df_new = df_new.assign(OPIOID_PRESCRIBED = df_new['RXNDC'].apply(lambda x: 1 if str(x)[0:9] in opioid_codes or str(x)[0:10] in opioid_codes else 0))
+        df_final = pd.concat([df_final, df_intermediate2])
+    
+    df_final.rename(col_dict_clean)
 
-    # rename cols at end?
+    return df_final
+
     # combine with other data
+    # Remove any non-opioid rows?
